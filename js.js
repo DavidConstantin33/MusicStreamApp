34ea8bec2eec04a2f82273410332b1ff84a2c4ea
David <142396942+DavidConstantin33@users.noreply.github.com>
2024-03-03 09:09:44 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+//Account Objects
 const acc1 = {
     name: 'David Constantin',
     username: 'DC',
@@ -14,6 +15,8 @@ const accounts = {
     acc1, acc2
 }
 
+//Variables
+const audio = new Audio();
 const loginModal = document.querySelector('.loginModal');
 const usernameLogin = document.querySelector('.username');
 const passwordLogin = document.querySelector('.password');
@@ -27,6 +30,70 @@ const B2Btn =document.querySelector('.B2');
 const RunBtn = document.querySelector('.runBtn');
 const heartBtn = document.querySelector('.heartBtn');
 const app = document.querySelector('.app');
+const playBtn = document.getElementById('playPauseBtn');
+const progressBar = document.getElementById('progress-bar');
+const currentTime = document.getElementById('timer');
+const progressContainer = document.getElementById('dragInput');
+let isPlaying = false;
+
+//                                      Functions
+
+const playPause = function() {
+    if (isPlaying) {
+        audio.pause();
+    } else {
+        if (audio.currentTime === audio.duration) {
+            // Reset the currentTime to 0 if the audio has reached the end
+            audio.currentTime = 0;
+        }
+        audio.play();
+    }
+    isPlaying = !isPlaying;
+};
+
+audio.addEventListener('ended', function() {
+    // Reset the currentTime to 0 when the audio reaches the end
+    audio.currentTime = 0;
+    isPlaying = false;
+
+    // Update the progress bar
+    progressBar.value = 0;
+});
+
+/////////                                DRAG                        ///////////
+progressBar.addEventListener('input', function() {
+    const percentage = progressBar.value / 100;
+    audio.currentTime = percentage * audio.duration;
+});
+
+progressContainer.addEventListener('mousedown', function(event) {
+    const totalWidth = progressContainer.clientWidth;
+    const clickX = event.clientX - progressContainer.getBoundingClientRect().left;
+    const percentage = clickX / totalWidth;
+
+    progressBar.value = percentage * 100;
+    audio.currentTime = percentage * audio.duration;
+
+    audio.pause();
+});
+
+progressContainer.addEventListener('mouseup', function() {
+    audio.play();
+});
+
+audio.addEventListener('timeupdate', function() {
+    progressBar.value = (audio.currentTime / audio.duration) * 100;
+
+    const minutes = Math.floor(audio.currentTime / 60);
+    const seconds = Math.floor(audio.currentTime % 60);
+    currentTime.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+});
+
+
+/////////
+
+
+//                                Login
 
 loginBtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -44,9 +111,14 @@ loginBtn.addEventListener('click', function (e) {
     console.log('Invalid username or password');
 });
 
+
+//                   Play buttons
+
 FLBtn.addEventListener('click', function() {
+    audio.src ='FlashingLights.mp3';
     songName.textContent='Flashing Lights';
     songImg.src='yeCover.jpg';
+    playPause();
 });
 
 B2Btn.addEventListener('click', function() {
@@ -63,3 +135,5 @@ heartBtn.addEventListener('click', function() {
     songName.textContent='Heartless';
     songImg.src='yeheart.png';
 });
+
+playBtn.addEventListener('click', playPause);
